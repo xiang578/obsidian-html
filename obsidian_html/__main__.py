9daba18ddf785010926413b05bc5da05cd3b253a
kmaasrud <kmaasrud@outlook.com>
2020-11-23 12:42:24 +0100

    Unecessary duplicate of the main function removed

--- obsidian_html/__main__.py
+++ obsidian_html/__main__.py
@@ -1,36 +1,3 @@
-import sys
-import argparse
-from .Vault import Vault
+from obsidian_html import main
 
-parser = argparse.ArgumentParser(
-    prog="obsidian-html",
-    description="Converts an Obsidian vault into HTML")
-
-parser.add_argument("Vault",
-                    metavar="vault",
-                    type=str,
-                    default=".",
-                    help="Path to the vault root")
-
-parser.add_argument("-o", "--output_dir",
-                    default=sys.argv[1] + "/html",
-                    help="Path to place the generated HTML")
-
-parser.add_argument("-t", "--template",
-                    default=None,
-                    help="Path to HTML template")
-
-parser.add_argument("-d", "--dirs",
-                    nargs="+",
-                    default=[],
-                    help="Extra sub-directories in vault that you want included")
-
-parser.add_argument("-f", "--filter",
-                    nargs="+",
-                    default=[],
-                    help="Filter notes by tags")
-
-args = parser.parse_args()
-
-vault = Vault(args.Vault, extra_folders=args.dirs, html_template=args.template, filter=args.filter)
-vault.export_html(args.output_dir)
+main()
